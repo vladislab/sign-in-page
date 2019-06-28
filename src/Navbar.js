ee2c1c6140b41a71d7c6d6084ead6169fd1e473f
Viet Nguyen <vmnguyen@ryerson.ca>
2019-06-28 16:46:39 -0400

    added Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import InputBase from "@material-ui/core/InputBase";
+import { withStyles } from "@material-ui/core/styles";
+import Switch from "@material-ui/core/Switch";
+import SearchIcon from "@material-ui/icons/Search";
+import styles from "./styles/NavbarStyle";
+
+class Navbar extends React.Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" color="primary">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit">
+              <span>FR</span>
+            </IconButton>
+            <Typography className={classes.title} variant="h6" color="inherit">
+              Log-in
+            </Typography>
+            <Switch />
+            <div className={classes.grow} />
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search..."
+                className={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(Navbar);
