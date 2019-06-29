9f733cbfad49dee2eecbe47b60ecd8c54e30303f
Viet Nguyen <vmnguyen@ryerson.ca>
2019-06-28 20:26:48 -0400

    added Context for Dark mode

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import React from "react";
 import "./App.css";
 import Navbar from "./Navbar";
 import Form from "./Form";
+import PageContent from "./PageContent";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Form />
-    </>
+    <ThemeProvider>
+      <PageContent>
+        <Navbar />
+        <Form />
+      </PageContent>
+    </ThemeProvider>
   );
 }
 
